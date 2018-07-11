8b85e8dc9f63f7b2d1775a7b3698d301965a4f9e
shaparen <ext-eugene.shaparenko@here.com>
2018-07-11 15:44:21 +0300

    added @SmokeTest tag

--- src/test/java/cucumber/runTest.java
+++ src/test/java/cucumber/runTest.java
@@ -8,6 +8,7 @@ import org.junit.runner.RunWith;
 @RunWith(Cucumber.class)
 @CucumberOptions(glue = "steps", //place were steps are defined
         format = {"pretty", "html:target/cucumber"},
-        features = {"src/test/java/features"}) //place where features located, it can be "classpath:features/annotation.feature"
+        features = {"src/test/java/features"},
+        tags = {"@SmokeTest"}) //place where features located, it can be "classpath:features/annotation.feature"
 
 public class runTest { }
