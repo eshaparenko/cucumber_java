a33ed77503d0618e02586f6642f4bcd0100f6a59
shaparen <ext-eugene.shaparenko@here.com>
2018-10-29 00:22:38 +0200

    Added test stage, fixes7

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-mvn clean test -Dcucumber.options="src/test/java/features --tags '@SmokeTest' --glue steps"
+mvn clean install -Dcucumber.options="src/test/java/features --tags '@SmokeTest' --glue steps"
