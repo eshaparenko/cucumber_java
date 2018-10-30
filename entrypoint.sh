321372864fa379f1f77fd3512975ece95854198f
shaparen <ext-eugene.shaparenko@here.com>
2018-10-30 17:50:54 +0200

    Added test stage, fixes8

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
-echo ============================================ Running Suite ============================================
-    mvn clean test -Dmaven.repo.local="/root/.m2" -Dcucumber.options="src/test/java/features --tags '@SmokeTest' --glue steps"
-echo ============================================ Tests Completed ==========================================
+#echo ============================================ Running Suite ============================================
+#    mvn clean test -Dmaven.repo.local="/root/.m2" -Dcucumber.options="src/test/java/features --tags '@SmokeTest' --glue steps"
+#echo ============================================ Tests Completed ==========================================
+echo "mvn version: "
+mvn --version
