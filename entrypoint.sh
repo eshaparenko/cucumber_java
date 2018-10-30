3f97dc3061cd11b8706808e5b6328dd42b28ee88
shaparen <ext-eugene.shaparenko@here.com>
2018-10-30 11:28:36 +0200

    Edited entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
-
+echo ============================================ Running Suite ============================================
 mvn clean install -Dcucumber.options="src/test/java/features --tags '@SmokeTest' --glue steps"
+echo ============================================ Tests Completed ==========================================
