ea4de81ca21025a7ce7da3c2146d7d50fdfdc344
shaparen <ext-eugene.shaparenko@here.com>
2018-10-30 11:30:47 +0200

    Edited entrypoint2

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 echo ============================================ Running Suite ============================================
-mvn clean install -Dcucumber.options="src/test/java/features --tags '@SmokeTest' --glue steps"
+mvn clean test -Dmaven.repo.local="/root/.m2" -Dcucumber.options="src/test/java/features --tags '@SmokeTest' --glue steps"
 echo ============================================ Tests Completed ==========================================
