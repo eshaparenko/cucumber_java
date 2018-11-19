88e2f8cdc6703b9684b054df2be3aca1b3211ae1
shaparen <ext-eugene.shaparenko@here.com>
2018-11-20 01:06:45 +0200

    webdriver helper changes10

--- src/test/java/cucumber/runTest.java
+++ src/test/java/cucumber/runTest.java
@@ -7,7 +7,7 @@ import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(glue = "steps", //place were steps are defined
-        format = {"pretty", "html:target/cucumber/", "json:target/cucumber-report/cucumber.json"},
+        format = {"pretty", "html:target/cucumber/", "json:target/cucumber.json"},
         features = {"src/test/java/features"},
         tags = {"@SmokeTest"}) //place where features located, it can be "classpath:features/annotation.feature"
 
